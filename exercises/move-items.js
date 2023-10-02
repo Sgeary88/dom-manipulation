8745b78c0df99dc716b24a6d39fcd6086375f359
Sgeary88 <97266345+Sgeary88@users.noreply.github.com>
2023-10-02 16:20:21 -0700

    first push

---
+++ exercises/move-items.js
@@ -0,0 +1,87 @@
+/**
+ * SORTING NODES WITHIN A CONTAINER
+ * Please, make sure to read the following files in the exercises-info folder before you start
+ * * 01 SelectNodes.md
+*/
+
+/**
+ * @task
+ * Select all elements that have class of "item" as a NodeList.
+ * Store them in the allItems variable
+ * Example: const allItems = <Your code>;
+ */
+
+	const allItems = document.querySelectorAll('.item');
+
+/**
+ * @task
+ * Select the main container by the id of "main"
+ * Store it in the main constant
+ * Example: const main = <Your code>
+ * */
+
+const main = document.getElementById('main');
+
+
+/**
+ * @task
+ * Select the favorites container by id of "favs"
+ * Store it in the favs constant
+ * Example: const favs = <Your code>;
+ */
+
+const favs = document.getElementById('favs');
+
+
+/**
+ * @task
+ * Create the updateCollections(id, direction) function that follows the list of requirements:
+ * Takes an argument of the item id (number)
+ * Take an argument of direction as a string value of 'toMain' or 'toFavs'
+ * Moves the element from the current parent to the new parent (from main to favs or vice versa)
+ * Changes the icon of the element: fa-heart-circle-plus for main, fa-heart-crack for favs items.
+ */
+
+// Create the updateCollections(id, direction) function that follows the list of requirements
+
+function updatedCollections (id, direction) {
+
+	const item = document.getElementById(id);
+
+	if (direction === 'toFavs') {
+		favs.appendChild(item);
+		item.querySelector('i').classList.remove('fa-heart-circle-plus');
+		item.querySelector('i').classList.add('fa-heart-crack');
+	} else if (direction === 'toMain') {
+		main.appendChild(item);
+		item.querySelector('i').classList.remove('fa-heart-crack');
+		item.querySelector('i').classList.add('fa-heart-circle-plus');
+	}
+};
+
+
+/**
+ * @task
+ * Iterate through the every item in allItems NodeList and apply the
+ * addEventListener click event to each item.
+ * The item click must execute/call the following:
+ * * Get the current item's parent id ('main' or 'favs')
+ * * Get the current item id (a number value)
+ * * Set the direction constant to be equal to 'toFavs' or 'toMain', based off the current location
+ * * The direction means the collection to move the item into, when the item is clicked
+ * * If the correct item's location is the parent of id 'main' -> the direction is 'toFavs'
+ * * If the correct item's location is the parent of id 'favs' -> the direction is 'toMain'
+ * * Make the updateCollections function call, assign the item Id and the direction defined above
+ */
+
+allItems.forEach(item => {
+	item.addEventListener('click', function () {
+		const itemId = item.id;
+		const isMain = item.parentElement.id === 'main';
+
+		const direction = isMain ? 'toFavs' : 'toMain';
+
+		updatedCollections(itemId, direction);
+
+	})
+})
